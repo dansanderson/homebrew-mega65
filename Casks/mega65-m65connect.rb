6124bc75cdf5235ae58d5679ace64788ab6f9591
Dan Sanderson <contact@dansanderson.com>
2024-07-06 00:04:14 -0700

    Typo in url-verified

--- Casks/mega65-m65connect.rb
+++ Casks/mega65-m65connect.rb
@@ -1,7 +1,7 @@
 cask "mega65-m65connect" do
     version "2.3"
     sha256 "1a43beee0d55a61f147594aedb053a1a3f525967b604c5fd7d4e9a103ce5f02c"
-    url "https://files.mega65.org/files/m/M65Connect-Mac-Universal_6saiwt.zip"
+    url "https://files.mega65.org/files/m/M65Connect-Mac-Universal_6saiwt.zip",
         verified: "https://files.mega65.org?id=5919a8b8-c23c-4616-9a52-37e077076638"
     name "M65Connect"
     desc "Transfer files and interact with the MEGA65 personal computer via LAN or serial USB (JTAG)"
